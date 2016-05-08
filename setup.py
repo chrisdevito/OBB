f20a464eda54a40fe6dfc493c25df3fd03872a51
chrisdevito <chrisdevito@chribis.com>
2016-05-08 18:18:15 -0400

    Readded history.rst

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ except ImportError:
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-# with open('HISTORY.rst') as history_file:
-#     history = history_file.read()
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
 
 requirements = [
     # TODO: put package requirements here
