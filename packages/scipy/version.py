8be6b93dd27dd71c2141822a8dcf00a14a2eeb4a
chrisdevito <chrisdevito@chribis.com>
2016-05-09 00:00:59 -0400

    Added packages folder.

---
+++ packages/scipy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '0.16.0'
+version = '0.16.0'
+full_version = '0.16.0'
+git_revision = 'Unknown'
+release = True
+
+if not release:
+    version = full_version
