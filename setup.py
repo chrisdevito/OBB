caf53cdd966e40209fb20e9d11bc49a357679b61
chrisdevito <chrisdevito@chribis.com>
2016-05-12 20:04:14 -0400

    upgrade the version.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('HISTORY.rst') as history_file:
 
 setup(
     name='OBB_Maya',
-    version='0.1.13',
+    version='0.1.14',
     description="Oriented Bounding Boxes in Maya.",
     long_description=readme,
     author="Christopher DeVito",
