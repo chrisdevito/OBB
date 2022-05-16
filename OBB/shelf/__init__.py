182e7b83ea420958f63c875a98cd67714a731dba
Chris Devito <Chris.Devito@methodstudios.com>
2022-05-16 11:34:04 -0400

    added new from points icon and added shelf button

--- OBB/shelf/__init__.py
+++ OBB/shelf/__init__.py
@@ -10,65 +10,86 @@ from collections import OrderedDict
 this_package = os.path.abspath(os.path.dirname(__file__))
 shelf_path = partial(os.path.join, this_package)
 
-buttons = OrderedDict({
-    'OBB_BoundingBox': {
-        'command': (
-            "from maya import cmds\n"
-            "from OBB.api import OBB\n"
-            "meshes = cmds.ls(selection=True)\n"
-            "if len(meshes) == 0:\n"
-            "   raise RuntimeError(\"Nothing selected!\")\n"
-            "for mesh in meshes:\n"
-            "   obbBoundBoxPnts = OBB.from_points(mesh)\n"
-            "   obbCube = cmds.polyCube(ch=False,\n"
-            "                           name=\"{}_BBOX\".format(mesh))[0]\n"
-            "   cmds.xform(obbCube, matrix=obbBoundBoxPnts.matrix)"
-        ),
-        'sourceType': 'python',
-        'style': 'iconOnly',
-        'image': shelf_path('OBB_boundingBox.png'),
-        'annotation': 'AOV Matte management',
-        'enableCommandRepeat': False,
-        'flat': True,
-        'enableBackground': False,
-    },
-    'OBB_Lattice': {
-        'command': (
-            "from maya import cmds\n"
-            "from OBB.api import OBB\n"
-            "meshes = cmds.ls(selection=True)\n"
-            "if len(meshes) == 0:\n"
-            "   raise RuntimeError(\"Nothing selected!\")\n"
-            "for mesh in meshes:\n"
-            "   obbBoundBoxPnts = OBB.from_points(mesh)\n"
-            "   lattice = cmds.lattice(dv=(2, 2, 2),\n"
-            "                          objectCentered=True,\n"
-            "                          name=\"{}_LATTICEBOX\".format(mesh))\n"
-            "   cmds.xform(lattice[1], matrix=obbBoundBoxPnts.matrix)\n"
-            "   cmds.xform(lattice[2], matrix=obbBoundBoxPnts.matrix)"
-        ),
-        'sourceType': 'python',
-        'style': 'iconOnly',
-        'image': shelf_path('OBB_lattice.png'),
-        'annotation': 'Oriented Bounding Box',
-        'enableCommandRepeat': False,
-        'flat': True,
-        'enableBackground': False,
-    },
-    'documentation': {
-        'command': (
-            "import webbrowser\n"
-            "webbrowser.open('https://obb.readthedocs.org')"
-        ),
-        'sourceType': 'python',
-        'style': 'iconOnly',
-        'image': shelf_path('OBB_docs.png'),
-        'annotation': 'documentation',
-        'enableCommandRepeat': False,
-        'flat': True,
-        'enableBackground': False,
+buttons = OrderedDict(
+    {
+        "OBB_BoundingBox": {
+            "command": (
+                "from maya import cmds\n"
+                "from OBB.api import OBB\n"
+                "meshes = cmds.ls(selection=True)\n"
+                "if len(meshes) == 0:\n"
+                '   raise RuntimeError("Nothing selected!")\n'
+                "for mesh in meshes:\n"
+                "   obbBoundBoxPnts = OBB.from_points(mesh)\n"
+                "   obbCube = cmds.polyCube(ch=False,\n"
+                '                           name="{}_BBOX".format(mesh))[0]\n'
+                "   cmds.xform(obbCube, matrix=obbBoundBoxPnts.matrix)"
+            ),
+            "sourceType": "python",
+            "style": "iconOnly",
+            "image": shelf_path("OBB_boundingBox.png"),
+            "annotation": "AOV Matte management",
+            "enableCommandRepeat": False,
+            "flat": True,
+            "enableBackground": False,
+        },
+        "OBB_BoundingBoxFromPoints": {
+            "command": (
+                "from maya import cmds\n"
+                "from OBB.api import OBB\n"
+                "meshes = cmds.ls(selection=True)\n"
+                "if len(meshes) == 0:\n"
+                '   raise RuntimeError("Nothing selected!")\n'
+                "obbBoundBoxPnts = OBB.from_selected_points(meshes[0])\n"
+                "obbCube = cmds.polyCube(ch=False,\n"
+                '                        name="{}_BBOX".format(meshes[0]))[0]\n'
+                "cmds.xform(obbCube, matrix=obbBoundBoxPnts.matrix)"
+            ),
+            "sourceType": "python",
+            "style": "iconOnly",
+            "image": shelf_path("OBB_boundingBoxFromPoints.png"),
+            "annotation": "AOV Matte management",
+            "enableCommandRepeat": False,
+            "flat": True,
+            "enableBackground": False,
+        },
+        "OBB_Lattice": {
+            "command": (
+                "from maya import cmds\n"
+                "from OBB.api import OBB\n"
+                "meshes = cmds.ls(selection=True)\n"
+                "if len(meshes) == 0:\n"
+                '   raise RuntimeError("Nothing selected!")\n'
+                "for mesh in meshes:\n"
+                "   obbBoundBoxPnts = OBB.from_points(mesh)\n"
+                "   lattice = cmds.lattice(dv=(2, 2, 2),\n"
+                "                          objectCentered=True,\n"
+                '                          name="{}_LATTICEBOX".format(mesh))\n'
+                "   cmds.xform(lattice[1], matrix=obbBoundBoxPnts.matrix)\n"
+                "   cmds.xform(lattice[2], matrix=obbBoundBoxPnts.matrix)"
+            ),
+            "sourceType": "python",
+            "style": "iconOnly",
+            "image": shelf_path("OBB_lattice.png"),
+            "annotation": "Oriented Bounding Box",
+            "enableCommandRepeat": False,
+            "flat": True,
+            "enableBackground": False,
+        },
+        "documentation": {
+            "command": (
+                "import webbrowser\n" "webbrowser.open('https://obb.readthedocs.org')"
+            ),
+            "sourceType": "python",
+            "style": "iconOnly",
+            "image": shelf_path("OBB_docs.png"),
+            "annotation": "documentation",
+            "enableCommandRepeat": False,
+            "flat": True,
+            "enableBackground": False,
+        },
     }
-})
+)
 
 
 def create_shelf():
@@ -82,19 +103,19 @@ def create_shelf():
         None
     """
 
-    tab_layout = mel.eval('$pytmp=$gShelfTopLevel')
-    shelf_exists = cmds.shelfLayout('OBB', exists=True)
+    tab_layout = mel.eval("$pytmp=$gShelfTopLevel")
+    shelf_exists = cmds.shelfLayout("OBB", exists=True)
 
     if shelf_exists:
-        cmds.deleteUI('OBB', layout=True)
+        cmds.deleteUI("OBB", layout=True)
 
-    shelf = cmds.shelfLayout('OBB', parent=tab_layout)
+    shelf = cmds.shelfLayout("OBB", parent=tab_layout)
 
     for button, kwargs in buttons.items():
 
-        img = QtGui.QImage(kwargs['image'])
-        kwargs['width'] = img.width()
-        kwargs['height'] = img.height()
+        img = QtGui.QImage(kwargs["image"])
+        kwargs["width"] = img.width()
+        kwargs["height"] = img.height()
 
         cmds.shelfButton(label=button, parent=shelf, **kwargs)
 
